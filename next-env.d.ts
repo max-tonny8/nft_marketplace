8d385eacb9cb1a8db6459fc49708181a90d182ee
tonny0831 <tonnyjansen0831@gmail.com>
2022-10-26 02:10:56 +0900

    initial commit

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,12 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-import { ethers } from "ethers";
-
-declare global {
-  interface Window {
-    ethereum?: ethers.providers.ExternalProvider;
-  }
-}
-
-export {};
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
