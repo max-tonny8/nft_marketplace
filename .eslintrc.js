5fd87e7d9a0fe871b1fb4b0c5f9ec30f7d3202cb
tonny0831 <tonnyjansen0831@gmail.com>
2022-10-26 14:49:18 +0900

    skip typecheck

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 /* eslint-env node */
 
 module.exports = {
-  // root: true,
-  // parser: "@typescript-eslint/parser",
-  // plugins: ["@typescript-eslint"],
-  // extends: [
-  //   "eslint:recommended",
-  //   "plugin:@typescript-eslint/recommended",
-  //   "prettier",
-  // ],
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
 };
